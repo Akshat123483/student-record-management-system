3e7a8f954f6fd82a00cfc3eb29595c07d10515a0
Akshat123483 <akshatsharma58715@gmail.com>
2026-02-21 18:52:28 +0530

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Student Record Management System" << endl;
+    cout << "Project Started Successfully" << endl;
+    return 0;
+}
